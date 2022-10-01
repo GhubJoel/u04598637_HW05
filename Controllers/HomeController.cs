6a51c38167bfbb0188ce291904a5b626c0f3efb8
U04598637 <u04598637@tuks.co.za>
2022-10-01 23:38:39 +0200

    Work on  book list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,27 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using u04598637_HW05.Models;
 
 namespace u04598637_HW05.Controllers
 {
     public class HomeController : Controller
     {
+        private DataService dataService = new DataService();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
--- Models/DataService.cs
+++ Models/DataService.cs
@@ -15,5 +15,29 @@ namespace u04598637_HW05.Models
         {
             ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         }
+
+        public List<BooksVM> getAllBooks()
+        {
+            List<BooksVM> books = new List<BooksVM>();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                using(SqlCommand cmd = new SqlCommand("Select * From books", con))
+                {
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            BooksVM bvm = new BooksVM
+                            {
+                                BookID = Convert.ToInt32(reader["bookid"]),
+                                BookName = Convert.ToString(reader["name"])
+                               
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
