f576a8f8ae6eadea9dd501c6d9830021f85200a9
U04598637 <u04598637@tuks.co.za>
2022-10-01 23:24:03 +0200

    Books view + Nav Bar

---
+++ Models/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace u04598637_HW05.Models
+{
+    public class DataService
+    {
+        private String ConnectionString;
+
+        public DataService()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+    }
+}
---
+++ Models/StudBookVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u04598637_HW05.Models
+{
+    public class StudBookVM
+    {
+        public List<Students> StudentsList { get; set; }
+        public List<BooksVM> BooksList { get; set; }
+    }
+}
